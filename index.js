df80d743098be3f1edeafb68741338dd31b5561c
Alex Lee <zhuoyanlee@gmail.com>
2017-04-28 15:26:57 +1000

    Node js setup for mockups

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
@@ -13,8 +13,11 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.get('/home', function(request, response) {
+  response.render('pages/home');
 });
 
 
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
